4dd191a4281faba5126cf4454dabfcd0e64189fb
eric.ty.liaw <eric.ty.liaw@0d055e58-e8f6-11dd-a971-7dbc132099af>
2009-03-19 00:32:44 +0000

    added threshold for each object

--- train.cpp
+++ train.cpp
@@ -112,7 +112,6 @@ int main(int argc, char *argv[])
 		cerr << "ERROR: could not train classifier" << endl;
 		exit(-1);
   }
-
   // save classifier configuration
 	if (configurationFile != NULL) {
 		if (!classifier.saveState(configurationFile)) {
