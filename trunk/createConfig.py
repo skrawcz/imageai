49c854e07fe7dd1150abeab7ed92c59d8897e255
stefan.krawczyk <stefan.krawczyk@0d055e58-e8f6-11dd-a971-7dbc132099af>
2009-03-19 08:21:29 +0000

    added scripts to do calculations.

---
+++ trunk/createConfig.py
@@ -0,0 +1,49 @@
+# file written by Stefan Krawczyk
+# randomly pics a number of lines from a language model
+import sys
+
+if len(sys.argv) < 7:
+    print("Error not enough arguments:\n")
+    sys.exit([-1])
+
+#for arg in sys.argv:
+#    print arg
+
+
+txtString = " \
+[ amount of frames to skip] \n \
+frameJump=5; \n \
+[the threshold upon which images of objects are considered] \n \
+isObjectThreshold=0.0; \n \
+[ the allowed overlap between two objects ] \n \
+boxOverlapThreshold=0.4; \n \
+[type of classification] \n \
+classifierType=%s; \n \
+\n \
+[if one half of training set is used to look at quality of results] \n \
+createGraph=1; [is a boolean, use 1 for true, 0 for false] \n \
+graphNames=boostResults; \n \
+graphTrainTestRatio=0.7; [percentage of examples used for training if createGraph = true ]\n \
+[the subset defines the proportion of images to use. 1= all images 10= one tenth etc ]\n \
+useSubset=1; \n \
+ \n \
+[boost options]\n \
+boostType=%s;   [CvBoost::DISCRETE = 0 CvBoost::REAL = 1  CvBoost::LOGIT = 2 CvBoost::GENTLE = 3 ] \n \
+boostWeakCount=%s; \n \
+boostWeightTrimRrate=%s; [from 0 to 1, inclusive] \n \
+boostMaxDepth=%s; \n \
+boostUseSurrogates=%s;  [is a boolean, use 1 for true, 0 for false] \n \
+\n \
+[this is where you set the feature type] \n \
+[HAAR= 0, HAARTILT = 1, HAAR_HAARTILT = 2,HOGN = 3, HOGUN = 4, HOGRI = 5] \n \
+[HAAR_HOG = 6, HCORNER = 7,	HAAR_HCORNER = 8, HOG_HCORNER = 8] \n \
+[HAAR_HOG_HCORNER = 9] \n \
+featureType=%s \n" % \
+(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6],sys.argv[7])
+
+fname = "configs/test.cfg"
+f = open(fname,'w')
+
+print("writing to file %s \n" %fname)
+f.write(txtString)
+f.close()
---
+++ trunk/testScript.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+echo "Setting script to run"
+
+CLASSIFIER="BOOST"
+BOOSTTYPE="0 1 3"
+BOOSTWEAKCOUNT="50 75 100 125 150"
+BOOSTTRIMWEIGHT="0.8 0.85 0.9 0.95"
+BOOSTMAXDEPTH="5 10 15 20"
+BOOSTUSESURROGATES="0 1"
+#FEATURETYPE="0 1 2 3 4 5 6 7 8 9 10"
+FEATURETYPE="0"
+COUNTER=0
+MACHINENAME="myth12"
+for classifier in $CLASSIFIER
+do
+		for boosttype in $BOOSTTYPE
+		do
+				for boostweakcount in $BOOSTWEAKCOUNT
+				do
+						for boosttrimweight in $BOOSTTRIMWEIGHT
+						do
+								for boostmaxdepth in $BOOSTMAXDEPTH
+								do
+										for boostusesurrogates in $BOOSTUSESURROGATES
+										do
+												for feature in $FEATURETYPE
+												do
+										 				echo "python createConfig.py $classifier $boosttype $boostweakcount $boosttrimweight $boostmaxdepth $boostusesurrogates $feature"
+														python createConfig.py $classifier $boosttype $boostweakcount $boosttrimweight $boostmaxdepth $boostusesurrogates $feature
+												
+														echo "nohup ./train -c tester.out config/test.cfg >>	$MACHINENAME.$feature.txt"
+														nohup ./train -c tester.out config/test.cfg >>	$MACHINENAME.$feature.txt
+														#nohup echo "monkey" >> $MACHINENAME.$feature.txt
+														let COUNTER=COUNTER+1
+												done
+										done
+								done
+						done
+				done
+		done
+done
+#
+#		nohup ./digit $Type $OneKImg $OneKLabel $Test1KImg $Test1KLabel $DEPTH $Q1DIR/onek.$DEPTH.txt >> nohupSingle.out
+#		echo "nohup ./digit singletree $OneKImg $OneKLabel $Test1KImg $Test1Klabel $DEPTH $Q1DIR/onek.$DEPTH.txt"
+#    let DEPTH=DEPTH+2 
+#done
+#echo "Copying Results to $Q1DIR/one"
+#mv -f results.singletree.* $Q1DIR/one
+#let DEPTH=4
+#while [  $DEPTH -lt $DEPTHMAX ]; do
+#		nohup ./digit singletree $TenKImg $TenKLabel $Test1KImg $Test1KLabel $DEPTH $Q1DIR/tenk.$DEPTH.txt >> nohupSingle10.out
+#		echo "nohup ./digit singletree $TenKImg $TenKLabel $Test1KImg $Test1KLabel $DEPTH $Q1DIR/tenk.$DEPTH.txt"
+#    let DEPTH=DEPTH+2 
+#done
+#echo "Copying Results to $Q1DIR/ten"
+#mv -f results.singletree.* $Q1DIR/ten
